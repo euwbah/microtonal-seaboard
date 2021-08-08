34077fd40484bfbd1c5a9d8589c655efe385389c
euwbah <euwbah@gmail.com>
2021-08-08 20:17:50 +0800

    Fixing no module found websockets.legacy

--- build.bat
+++ build.bat
@@ -4,7 +4,7 @@ py -m venv venv
 .\venv\Scripts\activate
 pip install --upgrade setuptools
 pip install -r requirements.txt
-pyinstaller -F -n microtonal-seaboard main.py &&^
+pyinstaller -F --hidden-import=websockets.legacy -n microtonal-seaboard main.py &&^
     cd dist/ &&^
     tar -a -c -f microtonal-seaboard-windows.zip ../mappings microtonal-seaboard.exe
 deactivate
