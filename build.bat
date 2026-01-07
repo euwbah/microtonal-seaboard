7970f6a807e8f09411c197b61f581e36ade0394d
euwbah <euwbah@gmail.com>
2026-01-07 19:01:25 +0800

    remove .venv, update ci/cd

--- build.bat
+++ build.bat
@@ -1,7 +1,14 @@
 REM use version 3.12 for now, seems like 3.13 is not supported by rtmidi yet.
-py -3.12 -m pip install --upgrade pip
-py -3.12 -m pip install --user virtualenv
-py -3.12 -m venv .venv
+py -3.12 --version >nul 2>&1
+if errorlevel 1 (
+    set PYTHON=python3.12
+) else (
+    set PYTHON=py -3.12
+)   
+
+%PYTHON% -m pip install --upgrade pip
+%PYTHON% -m pip install --user virtualenv
+%PYTHON% -m venv .venv
 call .\.venv\Scripts\activate
 python -m pip install --upgrade pip
 pip install --upgrade setuptools
